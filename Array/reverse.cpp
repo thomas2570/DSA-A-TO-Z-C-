e71305ddc00f763b5fd15392ea391ae88ae63dcf
thomas2570 <thomasramesh449@gmail.com>
2026-09-05 17:05:33 +0530

    commit: Update README with new installation instructions

---
+++ Array/reverse.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void printArr(int *arr, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << ",";
+    }
+    cout << endl;
+}
+int main()
+{
+    int arr[] = {5, 4, 3, 9, 2};
+    int n = sizeof(arr) / sizeof(int);
+
+    vector<int> copyArr(n);
+    for (int i = 0; i < n; i++)
+    {
+        int j = n - i - 1;
+        copyArr[i] = arr[j];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = copyArr[i];
+    }
+    printArr(arr, n);
+    return 0;
+}
